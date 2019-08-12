ba7a572d69914849fe0ebfcfc5bc3503cc415f5e
Tino Sabetta <tino.sabetta@gmail.com>
2019-08-12 20:13:26 +0200

    cleaned up this mess

--- js/kanban.js
+++ js/kanban.js
@@ -1,99 +1,90 @@
-function allowDrop(ev) {
-    ev.preventDefault();
+function allowDrop() {
+    event.preventDefault();
 }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id = "drag");
+function drag() {
+    event.dataTransfer.setData("text", event.target.id = "drag");
 }
 
-function drop(ev) {
-    ev.preventDefault();
-    ev.stopPropagation();
-    var data = ev.dataTransfer.getData("text");
+function drop() {
+    var data = event.dataTransfer.getData("text");
 
-    if (ev.target === document.body) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (event.target === document.body) {
         document.querySelector("#" + data).remove();
         return;
-    } else if (ev.target.className === "section") {
-        ev.target.appendChild(document.querySelector("#" + data));
-    } else if (ev.target.className === "item") {
-        ev.target.parentElement.appendChild(document.querySelector("#" + data));
-    } else if (ev.target === document.body) {
-        document.querySelector("#" + data).remove();
-        return;
+    } else if (event.target.className === "section") {
+        event.target.appendChild(document.querySelector("#" + data));
+    } else if (event.target.className === "item") {
+        event.target.parentElement.appendChild(document.querySelector("#" + data));
     }
     document.querySelector("#" + data).removeAttribute("id");
 }
 
-function makeNewTask(ev) {
-    var toDoSection = document.querySelector(".section");
-    var taskNameField = document.querySelector("#taskName");
-    var newItem = document.createElement("div");
+function makeNewTask(section, content) {
+    var taskNameField, newItem;
+
+    event.preventDefault();
+    taskNameField = document.querySelector("#taskName");
+    newItem = document.createElement("div");
+    if (content === "input") {
+        content = taskNameField.value;
+    }
     newItem.className = "item";
     newItem.draggable = true;
-    if (taskNameField.value) {
-        newItem.textContent = taskNameField.value;
-    } else {
-        return;
-    }
+    newItem.textContent = content;
     newItem.addEventListener("dragstart", drag);
-    toDoSection.appendChild(newItem);
-    ev.preventDefault();
-}
-
-function saveBoard() {
-    var items, i, sectionName;
-    items = document.querySelectorAll(".item");
-    localStorage.clear();
-    for (i = 0; i < items.length; i++) {
-
-        sectionName = items[i].parentElement.id;
-        console.log(sectionName);
-        localStorage.setItem(sectionName + "_" + i, items[i].textContent);
-    }
+    document.querySelector("#" + section).appendChild(newItem);
 }
 
 function loadBoard() {
     var i, x, sectioName, newItem;
+
     for (i = 0; i < localStorage.length; i++) {
         x = localStorage.key(i);
         sectionName = x.substr(0, x.indexOf('_'));
 
-        newItem = document.createElement("div");
-        newItem.className = "item";
-        newItem.draggable = true;
-        newItem.textContent = localStorage.getItem(x);
-        newItem.addEventListener("dragstart", drag);
-        document.querySelector("#"+sectionName).appendChild(newItem);
+        makeNewTask(sectionName, localStorage.getItem(x));
+    }
+}
+
+function saveBoard() {
+    var items, i, sectionName;
+
+    localStorage.clear();
+    items = document.querySelectorAll(".item");
+    for (i = 0; i < items.length; i++) {
+
+        sectionName = items[i].parentElement.id;
+        localStorage.setItem(sectionName + "_" + i, items[i].textContent);
     }
 }
 
 function init() {
-    var sections, items, i, formField, taskNameField;
+    var sections, items, i, formField, taskNameField, typeMessage;
 
     sections = document.querySelectorAll(".section");
     items = document.querySelectorAll(".item");
     formField = document.querySelector("form");
     taskNameField = document.querySelector("#taskName");
+    typeMessage = taskNameField.value;
 
     for (i = 0; i < sections.length; i++) {
         sections[i].addEventListener("drop", drop);
         sections[i].addEventListener("dragover", allowDrop);
     }
-
-    for (i = 0; i < items.length; i++) {
-        items[i].addEventListener("dragstart", drag);
-    }
     document.body.addEventListener("drop", drop);
     document.body.addEventListener("dragover", allowDrop);
+    formField.addEventListener("submit", makeNewTask.bind(null, "toDo", "input"));
 
-    formField.addEventListener("submit", makeNewTask);
     taskNameField.addEventListener("focus", function() {
         this.value = "";
     });
     taskNameField.addEventListener("blur", function() {
         if (this.value === "") {
-            this.value = "Here you can type in a new task";
+            this.value = typeMessage;
         }
     });
     window.addEventListener("unload", saveBoard);
